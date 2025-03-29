b90db09ad0f023a925fd779bf0426e44ac40f526
lajithaalagarbabu <lajit@lajithaalagarbabu@gmail.com>
2025-03-29 20:32:34 +0530

    resume changed

--- src/app/home/Header.jsx
+++ src/app/home/Header.jsx
@@ -7,7 +7,7 @@ import dribbble from "../../../public/dribbble-svgrepo-com.svg";
 const Header = () => {
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/Lajitha_resume.pdf"; // Access files in the public folder directly
+    link.href = "/lajitha-resume.pdf"; // Access files in the public folder directly
     link.download = "Lajitha-resume.pdf"; // Name for the downloaded file
     document.body.appendChild(link); // Append the link to the document
     link.click(); // Programmatically click the link
